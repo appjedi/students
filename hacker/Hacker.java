68e1930c0c9e71903f9984a0b44b9040a9a8e385
appjedi <appjedi.net@gmail.com>
2024-01-15 06:07:42 -0800

    update repo
    update repo

---
+++ hacker/Hacker.java
@@ -0,0 +1,85 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Hacker {
+
+    public static void main1(String[] args) throws java.lang.Exception {
+        Scanner kb = new Scanner(System.in);
+        int test_cases = kb.nextInt();
+        for (int cs = 1; cs <= test_cases; cs++) {
+            int n = kb.nextInt();
+            int a[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = kb.nextInt();
+            }
+            findZigZagSequence(a, n);
+        }
+    }
+
+    public static void main3(String[] args) {
+        int[] array = { 1, 2, 3, 4, 5, 6, 7 }; // Example array
+
+        // Step 1: Sort the array in ascending order
+        Arrays.sort(array);
+
+        // Step 2: Swap adjacent elements starting from the second element
+        for (int i = 1; i < array.length - 1; i += 2) {
+            int temp = array[i];
+            array[i] = array[i + 1];
+            array[i + 1] = temp;
+        }
+
+        // Display the lexicographically smallest zig-zag sequence
+        System.out.println("Lexicographically smallest zig-zag sequence: " + Arrays.toString(array));
+    }
+
+    public static void main(String[] args) {
+        // utopianTree(2, 4);
+        int[] a = { 1, 2, 3, 4, 5, 6, 7 };
+        int n = 7;
+        findZigZagSequence(a, n);
+    }
+
+    // 1 2 3 7 6 5 4
+    //
+    public static void findZigZagSequence(int[] a, int n) {
+        Arrays.sort(a);
+        int mid = (n) / 2; // 1
+        int temp = a[mid];
+        a[mid] = a[n - 1];
+        a[n - 1] = temp;
+
+        int st = mid + 1;
+        int ed = n - 2;
+        while (st <= ed) {
+            temp = a[st];
+            a[st] = a[ed];
+            a[ed] = temp;
+            st = st + 1;
+            // ed = ed - 1;
+        }
+        for (int i = 0; i < n; i++) {
+            if (i > 0)
+                System.out.print(" ");
+            System.out.print(a[i]);
+        }
+        System.out.println();
+    }
+
+    public static int letterCount(String str, char letter, int len) {
+        char[] letters = str.toCharArray();
+        int count = 0;
+        for (int i = 0; i < len; i++) {
+            if (letter == letters[i]) {
+                ++count;
+            }
+        }
+        return count;
+    }
+
+    public static int utopianTree(int start, int n) {
+        System.out.println("H: " + Math.pow(start, n));
+
+        return 1;
+    }
+}
---
+++ hacker/hacker.js
@@ -0,0 +1,102 @@
+/*
+https://www.youtube.com/watch?v=pVkHLciuank&list=PLSIpQf0NbcCltzNFrOJkQ4J4AAjW3TSmA&index=6
+*/
+
+const freeCodeCampBlog = new Map([
+  ['name', 'freeCodeCamp'],
+  ['type', 'blog'],
+  ['writer', 'Tapas Adhikary'],
+]);
+
+//console.log(freeCodeCampBlog.get('type'));
+freeCodeCampBlog.set('test', 'Someone else!');
+
+//console.log(freeCodeCampBlog);
+
+function countingSort(arr)
+{
+    const values = Array(100).fill(0);
+    for (let num of arr)
+    {
+        values[num]++;    
+    }
+    return values;
+}
+
+function flipMatrix1()
+{
+    const matrix = [[112, 42, 83, 119], [56, 125, 56, 49], [15, 78, 101, 43], [62, 98, 114, 108]];
+    console.log(matrix.length);
+    let rowMax = 0;
+    let colMax = 0;
+    let rowMaxIndex = -1;
+    let colMaxIndex = -1;
+    for (let i = 0; i < matrix.length; i++)
+    {
+        if (matrix[0][i] > colMax)
+        {
+            colMax = matrix[0][i];
+            colMaxIndex = i;
+        }    
+    }
+    for (let i = 0; i < matrix.length; i++)
+    {
+        if (matrix[0][i] > colMax)
+        {
+            colMax = matrix[0][i];
+            colMaxIndex = i;
+        }    
+    }
+}
+function flipMatrix() {
+    const matrix = [[112, 42, 83, 119], [56, 125, 56, 49], [15, 78, 101, 43], [62, 98, 114, 108]];
+    
+    const arr = Array(matrix.length);
+    const colIndex = parseInt(matrix.length/2);
+    let idx = 0;
+    for (let i = matrix.length-1; i >=0; i--)
+    {
+        arr[idx++] = matrix[i][colIndex];    
+    }
+    for (let i = 0;i< matrix.length; i++)
+    {
+        matrix[i][colIndex]=arr[i];    
+    }
+    idx = 0;
+    for (let i = matrix.length-1; i >=0; i--)
+    {
+        arr[idx++] = matrix[0][i];    
+    }
+    for (let i = 0;i< matrix.length; i++)
+    {
+        matrix[0][i]=arr[i];    
+    }
+    return matrix[0][0]+matrix[0][1]+matrix[1][0]+matrix[1][1];
+}
+
+
+function palindromeIndex(s) {
+    // Write your code here
+    if (s == s.split('').reverse().join(''))
+    {
+        return -1;    
+    }
+    const letters = s.split("");
+    const len = letters.length;
+    for (let i = 0; i < len/2; i++)
+    {
+        const endIndex = len - 1 - i;
+        if (letters[i] != letters[endIndex])
+        {
+            if (letters[i+1] != letters[endIndex])
+            {
+                return endIndex;
+            } else {
+                return i;
+            }
+       }    
+    }
+    return -1;
+} 
+console.log ( palindromeIndex("csbc"))
+//console.log(flipMatrix());
