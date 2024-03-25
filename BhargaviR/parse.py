ea7610c92d8588e66918e872742f52e26799cab5
Bob T <54815862+appjedi@users.noreply.github.com>
2024-03-25 11:49:04 -0700

    update repo
    update repo

--- BhargaviR/parse.py
+++ BhargaviR/parse.py
@@ -2,14 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 
 token1='s-product-image-container aok-relative s-text-center s-image-overlay-grey'
-
+content = ""
 def parse(url):
     response=requests.get(url)
     #text ='Hello' #response.content
     text = BeautifulSoup(response.text,'html.parser')
+    
     # find the index of is
-    token='is'
-    #print (response.content)
+    token='Name:'
+    print (response.content)
     
     result = text.index(token)
     print ("IndeX:", result)
@@ -17,14 +18,14 @@ def parse(url):
 def post():
     url = "http://www.armedicalboard.org/Public/directory/AdvancedDirectorySearch.aspx"
     values={"ctl00$MainContentPlaceHolder$lbBoxdirSearchLicType":"PA"}
-    myobj = {'somekey': 'somevalue'}
+
 
     x = requests.post(url, json = values)
 
     print(x.text)
 url = "https://www.amazon.com/s?k=sweaters&crid=2AXGFL9J3XCR2&sprefix=sweaters%2Caps%2C157&ref=nb_sb_noss_1"
-post(url)
-
+#post()
+parse("http://www.armedicalboard.org/public/directory/results.aspx?PHIDNO=ASMB212819")
 def test():
     text = 'Python is fun'
 
@@ -34,4 +35,3 @@ def test():
     print(result)
 
     # Output: 7
-
